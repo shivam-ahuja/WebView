e06cfd41bd7bee28b1ef5d74262cfbab81734e94
shivam ahuja <shivam.ahuja1996@gmail.com>
2017-08-08 17:53:18 +0530

    complete

---
+++ app/src/main/java/com/ahujasahab/webview/MainActivity.java
@@ -0,0 +1,100 @@
+package com.ahujasahab.webview;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private float m_downX;
+    private ProgressBar myProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+        setContentView(R.layout.activity_main);
+        myProgressBar=(ProgressBar)findViewById(R.id.myprogressbar);
+        webView=(WebView)findViewById(R.id.mywebview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.co.in/");
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                myProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                myProgressBar.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                myProgressBar.setVisibility(View.GONE);
+            }
+        });
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getPointerCount() > 1) {
+                    //Multi touch detected
+                    return true;
+                }
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        // save the x
+                        m_downX = event.getX();
+                    }
+                    break;
+
+                    case MotionEvent.ACTION_MOVE:
+                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_UP: {
+                        // set x so that it doesn't move
+                        event.setLocation(m_downX, event.getY());
+                    }
+                    break;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+        {
+            webView.goBack();
+        }
+       else  super.onBackPressed();
+    }
+}
